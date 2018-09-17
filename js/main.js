ad1af05f2879422b93ca9b7d1c526d517fd5d0c0
Artur Rymsza <artur.rymsza@gmail.com>
2018-09-17 19:28:22 +0200

    add second nav card

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
-function openNav() {
-    document.getElementById("menusidenav").style.width = "100%";
+var projectsnav = document.getElementById("projectssidenav");
+
+function openNav1() {
+    projectsnav.style.width = "100%";
 }
-function closeNav() {
-    document.getElementById("menusidenav").style.width = "30px";
+function closeNav1() {
+    document.getElementById("projectssidenav").style.width = "30px";
+}
+
+function openNav2() {
+    document.getElementById("aboutsidenav").style.width = "100%";
+}
+function closeNav2() {
+    document.getElementById("aboutsidenav").style.width = "60px";
 }
