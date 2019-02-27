480ac30e8d96c639fa1799ace5a8a2301fe556ca
Artur Rymsza <artur.rymsza@gmail.com>
2019-02-27 10:00:07 +0100

    Add basic prortfolio sidebar structure and Vue components

--- js/main.js
+++ js/main.js
@@ -14,3 +14,47 @@ function openNav2() {
 function closeNav2() {
     aboutnav.style.width = "0";
 }
+
+Vue.component('card',{
+template: `
+<div class="card">
+  <slot name="header"></slot>
+  <slot name="content"></slot>
+</div>
+`,
+data() {
+    return
+},
+});
+
+Vue.component('page',{
+    template: 
+    `<div>
+        <a href="javascript:void(0)" class="closebtn1" onclick="closeNav1()">x</a>
+        <div class="sidenav linesnav">
+            <card>
+                <h2 slot="header">Tytuł</h2>
+                <p slot="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </card>
+            <card></card>
+            <card></card>
+            <card></card>
+        </div>
+    </div>`,
+    data() {
+        test=false
+    },
+});
+
+var portfolio = new Vue({
+  el: '#app',
+  data: {
+  text:'testowy'
+  },
+  methods: {
+      changeText(){
+      this.text = 'nowy testowy'
+      }
+  }
+
+});
