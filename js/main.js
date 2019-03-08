b100c8e8eff6b7326cbe5081ab717c103821e466
Artur Rymsza <artur.rymsza@gmail.com>
2019-03-08 14:49:19 +0100

    Change style of projects sidenav

--- js/main.js
+++ js/main.js
@@ -17,24 +17,68 @@ function closeNav2() {
 
 Vue.component('card',{
 template: `
-<div class="card" :style="{transform: activeScale}" :class="{ project: openProject }" @mouseover="showText" @mouseleave="noText" @click="project">
-  <slot name="header"></slot>
-  <slot name="content"></slot>
-  <p>test</p>
-</div>
+<div class="card" :style="{transform: activeTransform}" :class="{ project: openProject }" 
+@mouseover="showText" @mouseleave="noText" @click="project">
+    <div class="content">
+        <div class="content-header">
+            <img class="content-logo" src="../img/logo-rozklad.png" alt="">
+            <img class="content-picture" src="../img/rozklad2.jpg" alt="">
+        </div>
+        <div class="content-description">
+            <h2>Interaktywny Rozkład Jazdy KM Kościerzyna</h2>
+            <p>Nulla fermentum ullamcorper justo sed aliquet. Nullam egestas sollicitudin rhoncus. 
+            In eget dolor vitae orci interdum luctus quis in nulla. Fusce eget lacinia ligula. 
+            Sed egestas, lacus sit amet iaculis maximus, quam arcu egestas quam, vitae eleifend est est eu risus. 
+            Etiam pharetra libero congue est bibendum varius. Cras tincidunt tellus quis felis imperdiet consectetur. 
+            Morbi id enim mauris. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
+            Donec in dolor vel velit porttitor cursus. Donec est mauris, lobortis vel tellus vitae, lobortis malesuada massa. 
+            Etiam a consectetur nisl, sit amet venenatis eros. Phasellus magna nunc, vestibulum in pretium sed, faucibus id felis. 
+            Ut et dapibus tortor.</p>
+        </div>
+        <div class="content-description2">
+            <img src="" alt="">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Morbi ullamcorper sapien erat, ut eleifend mi ultricies in. 
+            Duis at sapien ex. Quisque maximus mauris ut mi sodales luctus. 
+            Mauris vitae ligula et turpis feugiat vehicula vel nec leo. 
+            Aliquam aliquet auctor erat, ut maximus dolor consectetur et. 
+            In nec est auctor, ullamcorper enim sed, elementum odio. 
+            Sed ultricies nibh id egestas porta. Maecenas vehicula congue sollicitudin. 
+            Donec tristique lorem et neque auctor, ut sagittis enim ultrices. 
+            Duis gravida tincidunt odio. Integer ullamcorper volutpat tincidunt. 
+            Sed consequat scelerisque nulla. Nam nec auctor nunc.</p>
+        </div>
+        <div class="content-footer"></div>
+    </div>
+ </div>
 `,
 data() {
     return {
-        activeScale: 'scale(1)',
-        openProject: false
+        activeTransform: 'scale(1) skewX(-14deg)',
+        openProject: false,
+  /*      text: 'fajny',
+        brand: [
+            {
+              brandId: 1,
+              brandTitle: 'Interaktywny rozkład jazdy',
+              brandImage:  '../img/opinie.jpg',
+              brandLogo: '',  
+            },
+            {
+              variantId: 2235,
+              variantColor: 'blue',
+              variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg',
+              variantQuantity: 0     
+            }
+          ],*/
     }
 },
 methods: {
     showText() {
-        this.activeScale =  'scale(1.07)'
+        this.activeTransform =  'scale(1.07) skewX(-14deg)'
     },
     noText() {
-        this.activeScale =  'scale(1)'
+        this.activeTransform =  'scale(1) skewX(-14deg)'
     },
     project() {
         this.openProject = !this.openProject
@@ -48,8 +92,6 @@ Vue.component('page',{
         <a href="javascript:void(0)" class="closebtn1" onclick="closeNav1()">x</a>
         <div class="sidenav linesnav">
             <card :style="{backgroundImage: background1}">
-                <h2 slot="header">Tytuł test</h2>
-                <p slot="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
             </card>
             <card></card>
             <card></card>
